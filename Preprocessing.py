5762565820d9b42af9c1921667fb5aa65f1c8988
DylanLebatteux <67757611+DylanLebatteux@users.noreply.github.com>
2021-08-23 17:02:56 -0400

    Delete Preprocessing.py

--- Preprocessing.py
+++
@@ -1,8 +0,0 @@
-# Import
-from sklearn.preprocessing import MinMaxScaler
-
-# Function of MinMaxScaler (0, 1)
-def minMaxScaling(X):
-	minMaxScaler = MinMaxScaler(feature_range = (0, 1), copy = False)
-	X = minMaxScaler.fit_transform(X)
-	return X
