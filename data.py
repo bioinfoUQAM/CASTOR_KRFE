4b801d4b700ffdb1f5454d651efa13570958b2cb
DylanLebatteux <67757611+DylanLebatteux@users.noreply.github.com>
2022-06-07 11:22:10 -0400

    Add files via upload

--- data.py
+++ data.py
@@ -8,8 +8,18 @@ def loadData(file_path):
 	# Iterate through the fasta file
 	for record in Bio.SeqIO.parse(file_path, "fasta"):
 		# If there is a class label, save id, sequence and class label in the data list
-		try: D.append([record.id[0:record.id.index("|")], str(record.seq.upper()), record.id[record.id.index("|") + 1:]])
+		try: 
+			indexes = [i for i, c in enumerate(record.description) if c == "|"]
+			D.append([record.description, str(record.seq.upper()).replace('N',''), record.description[indexes[len(indexes)-1] +1 :]])
 		# If there is a no class label, save id and sequence in the data list
-		except: D.append([record.id, str(record.seq.upper())])
+		except: D.append([record.descrition, str(record.seq.upper())])
+	# Return the data matrix
+	return D
+
+# Function to load test data from a fasta file
+def loadReferenceSequence(D, reference_sequence_file):
+	# Iterate through the genbank file
+	for gb_record in  Bio.SeqIO.parse(open(reference_sequence_file,"r"), "genbank"):
+		D = [[gb_record.annotations["accessions"][0], str(gb_record.seq.upper()).replace('N',''), "Reference_sequence"]] + D
 	# Return the data matrix
 	return D
